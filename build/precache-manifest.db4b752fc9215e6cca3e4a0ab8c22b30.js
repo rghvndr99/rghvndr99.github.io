c1aa6b44aa1283f5e416abfcb5c4244642076f17
rdixi4 <rdixit5@sapient.com>
2019-02-05 21:02:58 +0530

    react build folder

---
+++ build/precache-manifest.db4b752fc9215e6cca3e4a0ab8c22b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48c620ee74b0a51e943d",
+    "url": "/static/js/main.48c620ee.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "48c620ee74b0a51e943d",
+    "url": "/static/css/main.0813d2d5.chunk.css"
+  },
+  {
+    "revision": "69edbb50a9acdc376caad8c4bd1b593c",
+    "url": "/index.html"
+  }
+];
